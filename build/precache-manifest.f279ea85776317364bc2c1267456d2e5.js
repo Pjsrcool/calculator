382b3791412c827bc9fcc3cb1e2e416b2ce8557e
Tara Black <elizabethhess.code@gmail.com>
2021-11-15 13:08:42 -0600

    add build folder

---
+++ build/precache-manifest.f279ea85776317364bc2c1267456d2e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70720ff033ffd28c91fce7664ddff97",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "aa1925bc8a18809ccf96",
+    "url": "/calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "98fdf273126e1055bad1",
+    "url": "/calculator/static/css/main.ee0acab7.chunk.css"
+  },
+  {
+    "revision": "aa1925bc8a18809ccf96",
+    "url": "/calculator/static/js/2.07e12a7c.chunk.js"
+  },
+  {
+    "revision": "98fdf273126e1055bad1",
+    "url": "/calculator/static/js/main.8733fdba.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
